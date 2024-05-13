168e19dbbfc3efaa0690cdd5818f49b74f3a82c3
Drunpy <rosalorran98@gmail.com>
2024-05-13 10:48:48 -0300

    Model de itens + Vetor logistico + ajustes

--- central_de_ajuda/abrigos/admin.py
+++ central_de_ajuda/abrigos/admin.py
@@ -1,7 +1,53 @@
 from django.contrib import admin
+
 from abrigos.models import *
-# Register your models here.
+
+
+class EnderecosDeAbrigosInline(admin.StackedInline):
+    model = Enderecos
+   
+    extra = 1
+    max_num = 1
+
+    exclude = [
+        "ultima_atualizacao"
+    ]
+
+class ItensDeAbrigosInline(admin.StackedInline):
+    model = Itens
+   
+    extra = 3 # TODO: Verificar pq não está sendo respeitado, o admin só deixa incluir 1 item no abrigo.
+    max_num = 50
+    exclude = [
+        "ultima_atualizacao"
+    ]
+
+
 class AbrigosAdmin(admin.ModelAdmin):
     model = Abrigos
+    inlines = (
+        EnderecosDeAbrigosInline,
+        ItensDeAbrigosInline,
+    )
+    exclude = [
+        "endereco",
+        "vetor_logistico",
+        "ultima_atualizacao"
+    ]
 
 admin.site.register(Abrigos, AbrigosAdmin)
+
+class TiposDeAbrigosAdmin(admin.ModelAdmin):
+    model = TiposDeAbrigo
+
+admin.site.register(TiposDeAbrigo, TiposDeAbrigosAdmin)
+
+class CategoriasDeItensAdmin(admin.ModelAdmin):
+    model = CategoriasDeItens
+
+admin.site.register(CategoriasDeItens, CategoriasDeItensAdmin)
+
+class ItensAdmin(admin.ModelAdmin):
+    model = Itens
+
+admin.site.register(Itens, ItensAdmin)
